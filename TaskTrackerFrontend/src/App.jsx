3e797fd1e7ca69330757e7f9e2a93eecfc211e8a
Palak <palakgupta792383@gmail.com>
2026-06-29 00:04:27 +0530

    fix cors issue

--- TaskTrackerFrontend/src/App.jsx
+++ TaskTrackerFrontend/src/App.jsx
@@ -7,7 +7,7 @@ import DashboardStats from "./components/DashboardStats";
 import Toast from "./components/Toast";
 import "./App.css";
 
-const API_URL = import.meta.env.VITE_API_URL + "/api/tasks";
+const API_URL = "https://task-tracker-a4nk.onrender.com/api/tasks";
 
 function App() {
   const [tasks, setTasks] = useState([]);
