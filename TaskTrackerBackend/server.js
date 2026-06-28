13c3162be6f88150712468eb1d44babc403c33b9
Palak <palakgupta792383@gmail.com>
2026-06-28 23:40:43 +0530

    update

--- TaskTrackerBackend/server.js
+++ TaskTrackerBackend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
+
 const taskRoutes = require("./routes/taskRoutes");
 
 dotenv.config();
@@ -11,6 +12,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("TaskTracker Backend is running 🚀");
+});
+
 app.use("/api/tasks", taskRoutes);
 
 const PORT = process.env.PORT || 5000;
@@ -18,7 +23,6 @@ const PORT = process.env.PORT || 5000;
 const startServer = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-
     console.log("MongoDB Connected");
 
     app.listen(PORT, () => {
